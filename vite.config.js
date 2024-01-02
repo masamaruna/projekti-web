5b9e2ce87a2887339bd8c151279e6c8060c8b5e0
Masa <masamaruna@gmail.com>
2024-01-02 17:55:34 +0100

    whoops, vite config whoops

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,12 @@ export default ({
             rollupOptions: {
                 input: {
                     main: resolve(__dirname, 'index.html'),
-                    koprivnica: resolve(__dirname, 'koprivnica.html')
+                    koprivnica: resolve(__dirname, 'koprivnica.html'),
+                    zagreb: resolve(__dirname, 'zagreb.html'),
+                    petrinja: resolve(__dirname, 'petrinja.html'),
+                    berlin: resolve(__dirname, 'berlin.html'),
+                    cologne01: resolve(__dirname, 'cologne-01.html'),
+                    cologne02: resolve(__dirname, 'cologne-02.html'),
                 }
             }
         }
