353d4f04ab35733fc252ca2e5c130d5e3d214b66
Masa <masamaruna@gmail.com>
2024-01-05 10:55:56 +0100

    updated vite to vite 5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-
 import { resolve } from 'path'
+import { defineConfig } from "vite";
 
-export default ({
+export default defineConfig({
         assetsInclude: ['**/*.glb'],
         build: {
             rollupOptions: {
